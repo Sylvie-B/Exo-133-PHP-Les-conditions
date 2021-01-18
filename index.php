2d699b762ca6d521e8a7d44767605a7520bb2aa8
Sylvie-B <unsolo@hotmail.fr>
2021-01-18 14:49:38 +0100

    php conditions

--- index.php
+++ index.php
@@ -1,47 +1,51 @@
 <?php
 
-// Premiere ligne
-$a = 50;
-$b = 10;
+    // Premiere ligne
+    $a = 50;
+    $b = 10;
 
-       >    {
-    echo "Hello World!";
-}
+    if($a > $b) {
+        echo "Hello World!";
+    }
 
-//Deuxieme ligne
-    {
-    echo "<br><br>Hello World2";
-}
+    //Deuxieme ligne
+    if($a !== $b) {
+        echo "<br><br>Hello World2";
+    }
 
-//Troisieme ligne
-($a==b) {
-    echo "<br><br>Oui";
-}
-
-{
-    echo "<br><br>Non";
-}
-
-
-//Quatrieme ligne
-echo"<br><br>";
- ($a == $b) {
-    echo "1";
-}  ($a > $b) {
-    echo "2";
-}
-{
-    echo "3";
-}
+    //Troisieme ligne
+    if($a === $b) {
+        echo "<br><br>Oui";
+    }
+    else {
+        echo "<br><br>Non";
+    }
 
 
-//Cinquieme ligne
-echo"<br><br>";
+    //Quatrieme ligne
+    echo"<br><br>";
 
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
-    echo "Welcome";
-}
+    if ($a == $b) {
+        echo "1";
+    }
+    elseif ($a > $b) {
+        echo "2";
+    }
+    else {
+        echo "3";
+    }
 
+
+    //Cinquieme ligne
+    echo"<br><br>";
+
+    $color = 'green';
+
+     switch($color) {
+        case "red":
+            echo "Hello";
+            break;
+       case "green":
+            echo "Welcome";
+            break;
+    }
